09eedd512a1b317c67fb87500fe07af21d7bec3c
minjeong <cpflqhdlrmsu@gmail.com>
2020-09-08 12:15:13 +0900

    modify page moving

---
+++ app/src/main/java/com/example/branchmemo/NewnoteActivity.java
@@ -0,0 +1,39 @@
+package com.example.branchmemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class NewnoteActivity extends AppCompatActivity {
+    Toolbar toolbar2;
+    public static ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newnote);
+
+        toolbar2 = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar2);
+        actionBar = getSupportActionBar();
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+        actionBar.setDisplayHomeAsUpEnabled(true); //툴바의 뒤로가기 버튼
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
