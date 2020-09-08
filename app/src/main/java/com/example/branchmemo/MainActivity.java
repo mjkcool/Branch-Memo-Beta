2f5a2a60147e72aca216406c4befdede594b0b83
minjeong <cpflqhdlrmsu@gmail.com>
2020-09-08 12:05:13 +0900

    modify page moving: using newnote.xml instead of fragment_first.xml and fragment_second.xml

--- app/src/main/java/com/example/branchmemo/MainActivity.java
+++ app/src/main/java/com/example/branchmemo/MainActivity.java
@@ -1,17 +1,13 @@
 package com.example.branchmemo;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Handler;
 import android.os.Message;
@@ -19,17 +15,15 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     public static ActionBar actionBar;
-
+    private static Handler mHandler ;
+    TextView Date_top_1, Date_top_2, Date_bottom;
 
     @SuppressLint("HandlerLeak")
     @Override
@@ -38,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
         actionBar = getSupportActionBar();
         actionBar = getSupportActionBar();
@@ -46,7 +39,50 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
         actionBar.setDisplayHomeAsUpEnabled(false); //툴바의 뒤로가기 버튼
 
+        Date_top_1 = findViewById(R.id.a_view);
+        Date_top_2 = findViewById(R.id.time_view);
+        Date_bottom = findViewById(R.id.date_view);
 
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Calendar cal = Calendar.getInstance() ;
+                SimpleDateFormat ap = new SimpleDateFormat("a");
+                SimpleDateFormat time = new SimpleDateFormat("hh:mm");
+                SimpleDateFormat date = new SimpleDateFormat("yyyy/MM/dd");
+                Date_top_1.setText(ap.format(cal.getTime()));
+                Date_top_2.setText(time.format(cal.getTime()));
+                Date_bottom.setText(date.format(cal.getTime()));
+            }
+        };
+
+        class NewRunnable implements Runnable {
+            Calendar cal = Calendar.getInstance();
+
+            @Override
+            public void run() {
+                while (true) {
+                    mHandler.sendEmptyMessage(0) ;
+                    try {
+                        Thread.sleep(1000) ;
+                    } catch (Exception e) {
+                        e.printStackTrace() ;
+                    }
+                }
+            }
+        }
+        NewRunnable nr = new NewRunnable() ;
+        Thread t = new Thread(nr) ;
+        t.start() ;
+
+
+        findViewById(R.id.newBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), NewnoteActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
