954f1c58ead38e66444e094baf27c4b3c969cba8
minjeong <cpflqhdlrmsu@gmail.com>
2020-11-02 13:43:49 +0900

    Make a new branch (SeungA)

--- app/src/main/java/com/example/branchmemo/viewnoteActivity.java
+++ app/src/main/java/com/example/branchmemo/viewnoteActivity.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -85,31 +86,24 @@ public class viewnoteActivity extends AppCompatActivity {
                 finish();
                 return true;
             case R.id.action_delete:
-                FragmentManager manager=getFragmentManager();
-                //(new FireMissilesDialogFragment(memoCode)).show(manager, "tag");
-                //Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(viewnoteActivity.this, MainActivity.class));
-                return true;
 
+                return true;
         }
         return super.onOptionsItemSelected(item);
-//        if (item.getItemId() == android.R.id.home) {//toolbar의 back키 눌렀을 때 동작
-//            finish();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
     }
 }
 
 class FireMissilesDialogFragment extends DialogFragment {
     String memoCode;
-    public FireMissilesDialogFragment(String code){
+    Context context;
+    public FireMissilesDialogFragment(String code, Context context){
+        this.context = context;
         this.memoCode = code;
     }
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
         builder.setMessage(R.string.dialog_message)
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
@@ -118,8 +112,9 @@ class FireMissilesDialogFragment extends DialogFragment {
                 })
                 .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-//                        MainActivity.DBModel.deleteMemo(memoCode);
-//                        MainActivity.DBModel.deleteMemoList(memoCode);
+                        MainActivity.DBModel.deleteMemo(memoCode);
+                        MainActivity.DBModel.deleteMemoList(memoCode);
+
                     }
                 });
         // Create the AlertDialog object and return it
